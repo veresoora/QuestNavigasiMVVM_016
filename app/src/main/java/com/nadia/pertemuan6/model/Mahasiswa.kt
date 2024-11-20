aa7c2a9753e63e32113e8edaefc92ff7f6afebc7
veresoora <lahanabira@gmail.com>
2024-11-20 23:32:06 +0700

    menambahkan notelp, email dan nim di data class Mahasiswa

--- app/src/main/java/com/nadia/pertemuan6/model/Mahasiswa.kt
+++ app/src/main/java/com/nadia/pertemuan6/model/Mahasiswa.kt
@@ -3,5 +3,8 @@ package com.nadia.pertemuan6.model
 data class Mahasiswa(
     val nama: String = "",
     val gender: String = "",
-    val alamat: String = ""
+    val alamat: String = "",
+    val noTelp: String = "",
+    val email: String = "",
+    val nim: String = ""
 )
