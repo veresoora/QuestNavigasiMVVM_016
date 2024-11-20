3b45af9a92eb88513276261fecb3daa8b06edd11
veresoora <lahanabira@gmail.com>
2024-11-21 00:43:53 +0700

    menambahkan NavHost pada Scaffold

--- app/src/main/java/com/nadia/pertemuan6/Navigation.kt
+++ app/src/main/java/com/nadia/pertemuan6/Navigation.kt
@@ -1,5 +1,6 @@
 package com.nadia.pertemuan6
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -8,6 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.NavigatorState
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.nadia.pertemuan6.ui.viewmodel.MahasiswaViewModel
 
@@ -21,8 +23,14 @@ fun Navigasi(
     modifier: Modifier = Modifier,
     viewModel: MahasiswaViewModel = viewModel(),
     navHost :NavHostController = rememberNavController()
-){
+) {
     Scaffold { isipadding ->
         val uiState by viewModel.dataModel.collectAsState()
+        NavHost(
+            navController = navHost,
+            startDestination = Halaman.Form.name,
+            modifier = modifier.padding(isipadding)
+        ) {
+        }
     }
 }
