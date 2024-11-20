dbd3768938a9f01110eac7ceba7219b089a0506d
veresoora <lahanabira@gmail.com>
2024-11-21 01:25:29 +0700

    mengubah greeting menjadi Navigasi

--- app/src/main/java/com/nadia/pertemuan6/MainActivity.kt
+++ app/src/main/java/com/nadia/pertemuan6/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Pertemuan6Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Navigasi(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
