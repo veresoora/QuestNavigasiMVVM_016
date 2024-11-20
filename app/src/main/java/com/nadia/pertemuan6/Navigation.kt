6acf824d4dcfaebb1a88b2defd8c739c499993ac
veresoora <lahanabira@gmail.com>
2024-11-21 00:41:26 +0700

    menambahkan Scaffold

--- app/src/main/java/com/nadia/pertemuan6/Navigation.kt
+++ app/src/main/java/com/nadia/pertemuan6/Navigation.kt
@@ -1,6 +1,9 @@
 package com.nadia.pertemuan6
 
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
@@ -19,5 +22,7 @@ fun Navigasi(
     viewModel: MahasiswaViewModel = viewModel(),
     navHost :NavHostController = rememberNavController()
 ){
-
+    Scaffold { isipadding ->
+        val uiState by viewModel.dataModel.collectAsState()
+    }
 }
