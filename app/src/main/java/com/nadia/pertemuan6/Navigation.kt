f94624a0dbfbb2b3e8a0224bdde973743ddfc917
veresoora <lahanabira@gmail.com>
2024-11-21 00:32:08 +0700

    membuat fungsi Navigasi beserta parameternya

--- app/src/main/java/com/nadia/pertemuan6/Navigation.kt
+++ app/src/main/java/com/nadia/pertemuan6/Navigation.kt
@@ -1,3 +1,23 @@
 package com.nadia.pertemuan6
 
-enum class Halaman{}
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.NavigatorState
+import androidx.navigation.compose.rememberNavController
+import com.nadia.pertemuan6.ui.viewmodel.MahasiswaViewModel
+
+enum class Halaman{
+    Form,
+    Data
+}
+
+@Composable
+fun Navigasi(
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost :NavHostController = rememberNavController()
+){
+
+}
