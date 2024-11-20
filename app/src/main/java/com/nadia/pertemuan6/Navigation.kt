8c75d437550ddb4b44053df7709c2c8a41dfaa4b
veresoora <lahanabira@gmail.com>
2024-11-21 01:12:10 +0700

    menambahkan navigasi ke halaman form

--- app/src/main/java/com/nadia/pertemuan6/Navigation.kt
+++ app/src/main/java/com/nadia/pertemuan6/Navigation.kt
@@ -6,11 +6,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.NavigatorState
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.nadia.pertemuan6.model.ListGender
+import com.nadia.pertemuan6.ui.view.FormMahasiswaView
 import com.nadia.pertemuan6.ui.viewmodel.MahasiswaViewModel
 
 enum class Halaman{
@@ -31,6 +35,18 @@ fun Navigasi(
             startDestination = Halaman.Form.name,
             modifier = modifier.padding(isipadding)
         ) {
+            composable(route = Halaman.Form.name){
+                val konteks = LocalContext.current
+                FormMahasiswaView(
+                    listGender = ListGender.listGender.map{
+                        isi -> konteks.resources.getString(isi)
+                    },
+                    onSubmitClick = {
+                        viewModel.saveDataMhs(it)
+                        navHost.navigate(Halaman.Data.name)
+                    }
+                )
+            }
         }
     }
 }
